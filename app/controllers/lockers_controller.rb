7288e26dc7767a1d75e877ab82bb9be7cb3ec203
Tamara Montijo <tamara.montijo@gmail.com>
2020-02-25 13:49:11 +0000

    added a container to log in and sign up page

--- app/controllers/lockers_controller.rb
+++ app/controllers/lockers_controller.rb
@@ -1,9 +1,47 @@
 class LockersController < ApplicationController
+
   def index
     @lockers = Locker.all
   end
 
   def show
     @locker = Locker.find(params[:id])
+  end
+
+  def new
+    @locker = Locker.new
+  end
+
+  def create
+    @locker = Locker.new(locker_params)
+
+    if @locker.save # => false / true
+      redirect_to @locker, notice: 'Locker was successfully created.'
+    else
+      render :new
+    end
+  end
+
+    def update
+    if @locker.update(locker_params) # true / false
+      redirect_to @locker, notice: 'Locker was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @locker.destroy
+    redirect_to lockers_url, notice: 'Locker was successfully deleted.'
+  end
+
+  private
+
+  def set_locker
+    @locker = Locker.find(params[:id])
+  end
+
+  def locker_params
+    params.require(:locker).permit(:size, :address, :price)
   end
 end
---
+++ app/policies/locker_policy.rb
@@ -0,0 +1,19 @@
+class LockerPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
