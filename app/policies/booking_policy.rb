9475f9447df4b047a7a6c330dad7a376b52e875c
Pedro Ventura <pedro.ventura19g@gmail.com>
2020-02-26 15:23:54 +0000

    Booking policy fixed

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,40 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  # only the user who created the restaurant is allowed to update it
+  def update?
+    # user - this is the current user
+    # record - this is the restaurant in this case because its a RestaurantPolicy
+    user_is_owner?
+  end
+
+  # only the user who created the restaurant is allowed to delete it
+  def destroy?
+    # user - this is the current user
+    # record - this is the restaurant in this case because its a RestaurantPolicy
+    user_is_owner?
+  end
+
+  private
+
+  def user_is_owner?
+    record.user == user
+  end
+
+  # def user_is_owner_or_admin?
+  #   record.user == user || user.admin?
+  # end
+end
+
