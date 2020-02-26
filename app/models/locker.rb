bc53697e4182c1ae8d0dd4a7cb5060bf5d5407ff
Leonardt Lauenstein <leonardt@outlook.com>
2020-02-26 23:02:34 +0000

    hover ocupacity

--- app/models/locker.rb
+++ app/models/locker.rb
@@ -2,13 +2,13 @@ class Locker < ApplicationRecord
   belongs_to :user
 
   validates :address, presence: true
-  validates :size, presence: true, inclusion: { in: ['S','M','L'] }
+  validates :size, presence: true, inclusion: { in: ['S - W40xH40x40cm','M - W70xH40x70cm','L - W90xH60x90cm'] }
   validates :price, presence: true
 
   def get_photo
-    if size == 'S'
+    if size == 'S - W40xH40x40cm'
       photo = "https://www.3dlockers.co.uk/_assets/img/site/products/large/3-door.jpg"
-    elsif size == 'M'
+    elsif size == 'M - W70xH40x70cm'
       photo = "https://www.3dlockers.co.uk/_assets/img/site/products/large/small-chemical-coshh-cabinet-900-x-460-x-460.jpg"
     else
       photo = "https://www.3dlockers.co.uk/_assets/img/site/products/large/1800high-security-cupboard.jpg"
