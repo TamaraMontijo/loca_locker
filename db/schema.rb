516652dd9587b66f6c3f37ea206e8ddb4e3c504c
Leonardt Lauenstein <leonardt@outlook.com>
2020-02-27 19:02:46 +0000

    map and booking

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2020_02_27_165949) do
 
+ActiveRecord::Schema.define(version: 2020_02_27_165949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
